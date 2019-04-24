ba60ed0e8e34cb8fcdb2c5a0299c609490b55743
dread <fluoridepsychosis@protonmail.com>
2019-04-24 18:13:38 +0100

    switched to CID identifier

--- script.py
+++ script.py
@@ -3,65 +3,70 @@
 import requests
 import json
 
-unii = "CD35PMG570" # User inputs a unique chemical identifier called a UNII
+cid = input("Enter your CID:") # User inputs a unique chemical identifier called a CID
 
-url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" +unii+ "/synonyms/json" # This constructs the url for the PubChem API
+url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/" + cid + "/JSON?heading=mesh+entry+terms" # This constructs the url for the PubChem API
 
-r = requests.get(url) # Here we are recovering the data from the API
 
-synonyms_json = r
+synonyms_json = requests.get(url) # Here we are recovering the data from the API
 
 synonyms_list = synonyms_json.json() # Transforming the JSON data into a Python array for parsing
 
 # The following block of code parses the nested arrays down to just a single list of synonyms for the drug.
 
-synonyms_list = synonyms_list['InformationList']
-
-synonyms_list = synonyms_list['Information']
-
-synonyms_list = synonyms_list[0]
-
-synonyms_list = synonyms_list['Synonym']
+synonyms_list = synonyms_list['Record']['Section'][0]['Section'][0]['Section'][0]['Information'][0]['Value']['StringWithMarkup']
 
 big_list = []
 
-for synonym in synonyms_list:
+for synonymdict in synonyms_list:
 
-    # First we create the Entrez API query URL
-    entrez_url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&reldate=1&retmax=1000&retmode=json&term=" + synonym
+    for key, synonym in synonymdict.items():
 
-    # Next we query the API for a list of papers which match that synonym
-    entrez_response = requests.get(entrez_url)
+        # First we create the Entrez API query URL
+        entrez_url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&reldate=1&retmax=1000&retmode=json&term=" + synonym
+
+        # Next we query the API for a list of papers which match that synonym
+        entrez_response = requests.get(entrez_url)
+        
+        # We convert the list to a python array from json
+        parsed_json = entrez_response.json()
+
+        key = 'esearchresult'
+
+        name = synonym
+
+        # This loop finds whether the json contains the term 'esearchresult' as a key, then appends the value associated with that key
+        # (which is a list of pubmed IDs) to a larger, nested list called big_list
+        if key in parsed_json:
+
+            list_of_pmids = parsed_json["esearchresult"]["idlist"]
+
+            namedict = {}
+
+            for item in list_of_pmids:
+                namedict[item] = name
     
-    # We convert the list to a python array from json
-    parsed_json = entrez_response.json()
-
-    key = 'esearchresult'
-
-    # This loop finds whether the json contains the term 'esearchresult' as a key, then appends the value associated with that key
-    # (which is a list of pubmed IDs) to a larger, nested list called big_list
-    if key in parsed_json:
-
-        list_of_pmids = parsed_json["esearchresult"]["idlist"]
-
-        big_list.append(list_of_pmids)
-    
-    else:
-        pass
+        
+        else:
+            pass
+        
+        big_list.append(namedict)
 
 # Flattening the big_list
-flat_array = [item for sublist in big_list for item in sublist]
+flat_dictionary = {}
 
-# Removing duplicates from the array
-flat_array = list(dict.fromkeys(flat_array))
+for dictionary in big_list:
+    
+    flat_dictionary.update(dictionary)
 
-for item in flat_array:
+
+for key, item in flat_dictionary.items():
 
     # This loop is basically the same as the first one except it uses entrez eutils summary instead of eutils search to find info from the PMID,
     # then prints it to the output file
 
     # Creating the API query URL
-    entrez_summary_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=" + item
+    entrez_summary_url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=" + key
 
     # Obtaining the summary for that PMID
     entrez_summary_response = requests.get(entrez_summary_url)
@@ -70,7 +75,7 @@ for item in flat_array:
     summary_parsed_json = entrez_summary_response.json()
 
     # Converting the PMID to a string for concatenation to a URL
-    pmid = str(item)
+    pmid = str(key)
     
     pmid_key = "{}".format(pmid)
 
@@ -100,7 +105,7 @@ for item in flat_array:
 
         
         # Prints paper title, PMID and pubmed URL in a human-readable form
-        print(title + " URL: " + pubmed_url  + " DOI: " + doi)
+        print("[{}] ".format(item) + title + " URL: " + pubmed_url  + " DOI: " + doi)
 
     else: 
         pass
